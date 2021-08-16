07147ff103d8322e3ed69c4b69e1efb0d167fabd
tom_glb <goodMorning_glb@atguigu.com>
2021-08-16 18:54:58 +0800

    third commit

--- src/test/java/com/wuxin/SpringbootDemoApplicationTests.java
+++ src/test/java/com/wuxin/SpringbootDemoApplicationTests.java
@@ -2,12 +2,9 @@ package com.wuxin;
 
 import com.wuxin.dao.DepartmentDao;
 import com.wuxin.dao.EmployeeDao;
-import com.wuxin.pojo.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Collection;
 
 @SpringBootTest
 class SpringbootDemoApplicationTests {
@@ -20,11 +17,10 @@ class SpringbootDemoApplicationTests {
     @Test
     void contextLoads() {
 
-        Collection<Employee> employees = employeeDao.getAll();
-        System.out.println(employees);
         System.out.println("hello git!");
         System.out.println("hello git!!");
         System.out.println("hello git!!!");
+        System.out.println("hello git! master");
 
     }
 
